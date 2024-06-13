f6a0defe5d155b9a09f05b4f2a098df64ae387ab
TomaszSuchan <9158028+TomaszSuchan@users.noreply.github.com>
2024-06-13 21:24:00 +0200

    Change to load original AFLP matrices (remove first row and duplicated rows), allow defining strata column from population data file

--- R/AFLP_plot_PCoA.R
+++ R/AFLP_plot_PCoA.R
@@ -3,31 +3,35 @@
 #' Plot PCoA from AFLP data (individuals with missing data are removed!), ggplot-based so can be styled with additional ggplot arguments, eg. AFLP_plot_PCoA + theme(axis.text = element_text(size = 10))
 #' @import ggplot2
 #' @import vegan
-#' @param aflp_matrix_path AFLP matrix where rows reperesent individuals and columns the markers, no header, first column are individual names in the format Population-Individual (eg. POP1-1), if any additional column should be ommited (eg. with population number) it should be passed using the 'remove' argument.
+#' @param aflp_matrix_path AFLP matrix where rows reperesent individuals and columns the markers, no header, first column are individual names in the format Population-Individual (eg. POP1-1), if any additional column should be ommited (eg. with population number) it should be passed using the 'remove' argument. Assumes that the first row should be removed and duplicated rows as well (that's how the structure input is usually coded for AFLP).
 #' @param population_data_path path for the population data file where the first column is the population and the second the strata. Can have a header as long it does not contain any of the population names!
+#' @param strata coulumn in the population data file used for data stratification
 #' @param choices vector containing the PCoA axes to be plotted
-#' @param remove vector of colums to be removed
+#' @param remove vector of colums to be removed, defalults to usual population column in structure files. Set as remove=NULL if no column should be removed.
 #' @export
 #' @examples
 #' aflp_matrix_path <- "struct-new/original_input/Cal_Cal_Structure.txt"
 #' population_data_path <- "population_data.txt"
 #' AFLP_plot_PCoA(aflp_matrix_path, population_data_path, c(2,3))
 
-AFLP_plot_PCoA <- function(aflp_matrix_path, population_data_path=NULL, choices=c(1,2), remove=2) {
+AFLP_plot_PCoA <- function(aflp_matrix_path, population_data_path=NULL, strata = 1, choices=c(1,2), remove=2) {
    
-  aflp_matrix <- read.table(aflp_matrix_path, head=T, sep="\t")
-  colnames(aflp_matrix)[1] <- "Individual"
+  aflp_matrix <- read.table(aflp_matrix_path, head=FALSE, sep="\t", row.names=NULL, skip=1)
+  
+  # hack to remove last column full of NA
+  aflp_matrix <- aflp_matrix[,colSums(is.na(aflp_matrix))<nrow(aflp_matrix)]
 
-  # Rename the markers
-  colnames(aflp_matrix) <- gsub(".", "-", colnames(aflp_matrix), fixed=T)
+  # Remove duplicated rows (as in Structure input files)
+  aflp_matrix <- aflp_matrix[!duplicated(aflp_matrix), ]
+
+  colnames(aflp_matrix)[1] <- "Individual"
 
   # Extract population information from individual names
   split_names <- strsplit(as.character(aflp_matrix$Individual), "-")
   Population <- sapply(split_names, function(x) x[1])
   aflp_matrix <- cbind(Population, aflp_matrix)
 
-  # Remove duplicated rows (as in Structure input files)
-  aflp_matrix <- aflp_matrix[!duplicated(aflp_matrix), ]
+
 
   # Remove rows (samples) containing NA in AFLP data
   aflp_matrix[aflp_matrix==-9] <- NA
@@ -38,6 +42,8 @@ AFLP_plot_PCoA <- function(aflp_matrix_path, population_data_path=NULL, choices=
   cols1 <- names(cols1)
   cols0 <- colSums(aflp_matrix[, -c(1, 2, remove)])[colSums(aflp_matrix[, -c(1, 2, remove)]) == 0]
   cols0 <- names(cols0)
+  print("Removing columns with monomorphic markers:")
+  print(c(cols1, cols0))
   if(length(cols1)>0){
     aflp_matrix <- aflp_matrix[,!(names(aflp_matrix) %in% cols1)]
   }
@@ -55,7 +61,7 @@ AFLP_plot_PCoA <- function(aflp_matrix_path, population_data_path=NULL, choices=
                        population_data,
                        by.x="aflp_matrix$Population",
                        by.y="V1")
-  scores <- cbind(populations$V2, as.data.frame(scores(PCoA, display = "sites", choices=choices)))
+  scores <- cbind(populations[,strata], as.data.frame(scores(PCoA, display = "sites", choices=choices)))
   colnames(scores)[1] <- "Group"
   nmds_plot_new <- ggplot(scores, aes_string(x = names(scores)[2], y = names(scores)[3], fill = names(scores)[1])) +
     coord_fixed() +
