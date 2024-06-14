8b3bfef9778cfb39e5d562aab84ca89a9e048827
TomaszSuchan <9158028+TomaszSuchan@users.noreply.github.com>
2024-06-14 15:52:28 +0200

    Get sample data from input files

--- R/prepare_str_mapdata.R
+++ R/prepare_str_mapdata.R
@@ -6,51 +6,73 @@
 #' @import pophelper
 #' @import mapmixture
 #' @param structure_output_path path for the directory with structure output files
+#' @param structure_input_path path for the structure input file or plink .fam file
 #' @param population_data_path path for the population data file where the first column is the population. Can have a header as long it does not contain any of the population names!
 #' @param filetype type of the input data ('auto', 'structure','tess2','baps','basic' or 'clumpp')
 #' @param remove vector of colums to be removed
 #' @export
 #' @examples
-#' population_data_path <- "population_data.txt"
-#' structure_output_path <- "struct-new/output/Pmi"
-#' strmapdata <- prepare_str_mapdata(structure_output_path, population_data_path)
-#' boundary <- c(xmin=5, xmax=27.5, ymin=43.5, ymax=50)
+#' # For finestructure data:
+#' population_data_path <- "test_data/fastStructure/Pta_population_data.txt"
+#' structure_output_path <- "test_data/fastStructure/meanQ"
+#' structure_input_path <- "test_data/fastStructure/Pta_plink.fam"
+#' strmapdata <- prepare_str_mapdata(structure_output_path, structure_input_path, population_data_path, filetype="basic")
+#' boundary <- c(xmin=18, xmax=21, ymin=48.5, ymax=49.5)
 #' earth <- terra::rast("~/GIS/NE1_HR_LC_SR_W_DR/NE1_HR_LC_SR_W_DR.tif")
 #' gg_color_hue <- function(n) {
 #'   hues = seq(15, 375, length = n + 1)
 #'   hcl(h = hues, l = 65, c = 100)[1:n]
 #' }
-#' k <- 2
-#' mapmixture(strmapdata$str[[k]], strmapdata$pop, pie_size = 0.5, basemap = earth, boundary = boundary, cluster_cols = gg_color_hue(k), arrow_position = "tr")
+#' k <- 3
+#' mapmixture(strmapdata$str[[as.character(k)]], strmapdata$pop, pie_size = 0.1, basemap = earth, boundary = boundary, cluster_cols = gg_color_hue(k), arrow_position = "tr")
+#' 
+#' # For structure data:
+#' population_data_path <- "test_data/structure/popdata.txt"
+#' structure_output_path <- "test_data/structure/Aal_admix_out"
+#' structure_input_path <- "test_data/structure/Aal_carp_structure-inputD.txt"
+#' strmapdata <- prepare_str_mapdata(structure_output_path, structure_input_path, population_data_path, filetype="structure")
+#' boundary <- c(xmin=16, xmax=27.5, ymin=43.5, ymax=50)
+#' earth <- terra::rast("~/GIS/NE1_HR_LC_SR_W_DR/NE1_HR_LC_SR_W_DR.tif")
+#' gg_color_hue <- function(n) {
+#'   hues = seq(15, 375, length = n + 1)
+#'   hcl(h = hues, l = 65, c = 100)[1:n]
+#' }
+#' k <- 3
+#' mapmixture(strmapdata$str[[as.character(k)]], strmapdata$pop, pie_size = 0.5, basemap = earth, boundary = boundary, cluster_cols = gg_color_hue(k), arrow_position = "tr")
 
 
-prepare_str_mapdata <- function(structure_output_path, population_data_path, filetype="structure") {
+prepare_str_mapdata <- function(structure_output_path, structure_input_path, population_data_path, filetype="structure") {
   # Structure
   sfiles <- list.files(structure_output_path, full.names = T)
-  slist <- readQ(sfiles, filetype=filetype, indlabfromfile = T)
+  slist <- readQ(sfiles, filetype=filetype, indlabfromfile = F)
  
   #clumpp-like:
   aligned_slist <- alignK(slist)
   split_slist <- splitQ(aligned_slist)
-  reduceApplyListOfArrays<- function(x){
-         y<-apply(array(unlist(x), c(dim(x[[1]]), dim(x[[2]]), length(x))),
- 
-                c(1,2), mean)
-       colnames(y)<-colnames(x[[1]])
-       rownames(y)<-rownames(x[[1]])
-    return(y)
+
+  if(filetype=="structure"){
+    reduceApplyListOfArrays<- function(x){
+         y<-apply(array(unlist(x), c(dim(x[[1]]), dim(x[[2]]), length(x))), c(1,2), mean)
+         colnames(y)<-colnames(x[[1]])
+         rownames(y)<-rownames(x[[1]])
+      return(y)
+    }
+    merged_slist <- lapply(split_slist, reduceApplyListOfArrays)
   }
-  merged_slist <- lapply(split_slist, reduceApplyListOfArrays)
+  else {
+    merged_slist <- split_slist 
+  }
+  
   addPopdata <- function(x){
-    Ind <- rownames(x)
+    Ind <- read.table(structure_input_path, row.names=NULL)[1]
+    Ind <- Ind[!duplicated(Ind), ]
     split_names <- strsplit(as.character(Ind), "-")
     Site <- sapply(split_names, function(x) x[1])
     x <- cbind(Site, Ind, as.data.frame(x))
     return(x)
   }
   merged_slist_popdata <- lapply(merged_slist, addPopdata)
-  
-  Ind <- rownames(readQ(sfiles[1], filetype="structure", indlabfromfile = T)[[1]])
+
   # Extract population information from individual names
   split_names <- strsplit(as.character(Ind), "-")
   Site <- sapply(split_names, function(x) x[1])
