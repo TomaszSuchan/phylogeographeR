429b47a356d2ceedde85ab46fedd714bbed75f24
TomaszSuchan <9158028+TomaszSuchan@users.noreply.github.com>
2024-06-27 19:08:54 +0200

    Add tiplabel option, update docs

--- R/AFLP_plot_NJtree.R
+++ R/AFLP_plot_NJtree.R
@@ -5,11 +5,11 @@
 #' @import ggtree
 #' @import ape 
 #' @import vegan
-#' @param aflp_matrix_path AFLP matrix where rows reperesent individuals and columns the markers, no header, first column are individual names in the format Population-Individual (eg. POP1-1), if any additional column should be ommited (eg. with population number) it should be passed using the 'remove' argument. Assumes that the first row should be removed and duplicated rows as well (that's how the structure input is usually coded for AFLP).
+#' @param structure_input_path AFLP matrix where rows reperesent individuals and columns the markers, no header, first column are individual names in the format Population-Individual (eg. POP1-1), if any additional column should be ommited (eg. with population number) it should be passed using the 'remove' argument. Assumes that the first row should be removed and duplicated rows as well (that's how the structure input is usually coded for AFLP).
 #' @param population_data_path path for the population data file (optional) where the first column is the population, second is  and the second the strata. Can have a header as long it does not contain any of the population names!
 #' @param strata coulumn in the population data file used for data stratification
-#' @param axes vector containing the PCoA axes to be plotted
 #' @param remove vector of colums to be removed, defalults to usual population column in structure files. Set as remove=NULL if no column should be removed.
+#' @param labels should tip labels be printed or not
 #' @export
 #' @examples
 #' structure_input_path <- "test_data/structure/Aal_carp_structure-inputD.txt"
@@ -17,7 +17,7 @@
 #' AFLP_plot_NJtree(structure_input_path, population_data_path, strata=5, remove=NULL)
 
 
-AFLP_plot_NJtree <- function(structure_input_path, population_data_path=NULL, strata=4, remove = NULL) {
+AFLP_plot_NJtree <- function(structure_input_path, population_data_path=NULL, strata=4, remove = NULL, labels = TRUE) {
   aflp_matrix <- load_AFLP(structure_input_path, remove)
   rownames(aflp_matrix) <- aflp_matrix[,1]
   aflp_matrix <- as.matrix(aflp_matrix[, -1])
@@ -27,7 +27,7 @@ AFLP_plot_NJtree <- function(structure_input_path, population_data_path=NULL, st
   tr <- nj_tree(aflp_matrix)
 
 
-  tr$node.label <- boot.phylo(tr, aflp_matrix, nj_tree, B=1000)
+  #tr$node.label <- boot.phylo(tr, aflp_matrix, nj_tree, B=1000)
 
   #add popdata
 
@@ -52,12 +52,11 @@ AFLP_plot_NJtree <- function(structure_input_path, population_data_path=NULL, st
   
   tr <- groupOTU(tr, groups, "Group")
 
-  tree_plot <- ggtree(tr, aes(color=group), layout="daylight") + 
-               geom_tiplab(color="black") 
+  tree_plot <- ggtree(tr, aes(color=group), layout="daylight")
 
   } else {
-  tree_plot <- ggtree(tr, layout="daylight") + 
-               geom_tiplab(color="black")
+  tree_plot <- ggtree(tr, layout="daylight")
   }
+  geom_tiplab(color="black") 
   return(tree_plot)
 }
